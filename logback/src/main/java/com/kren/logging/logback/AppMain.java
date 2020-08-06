c02dc8862375fa8750b715c11907b9aa1719b7ec
krenMaksim <krenmaba@gmail.com>
2020-08-06 07:35:45 +0300

    add logback module

---
+++ logback/src/main/java/com/kren/logging/logback/AppMain.java
@@ -0,0 +1,12 @@
+package com.kren.logging.logback;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class AppMain {
+	private static Logger logger = LoggerFactory.getLogger(AppMain.class);
+
+	public static void main(String[] args) {
+		logger.debug("Hello world.");
+	}
+}
